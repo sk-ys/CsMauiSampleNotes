34fccf0707f008e2ee55756068ad4aa5bb975cd1
sk-ys <yskrmt@gmail.com>
2024-05-07 22:31:41 +0900

    Load and save a note - 1

--- Notes/NotePage.xaml.cs
+++ Notes/NotePage.xaml.cs
@@ -2,6 +2,8 @@ namespace Notes;
 
 public partial class NotePage : ContentPage
 {
+	string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
 	public NotePage()
 	{
 		InitializeComponent();
