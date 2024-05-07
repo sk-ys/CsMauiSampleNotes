39961cd2be64e2c8f84c86aae93237a47e22249a
sk-ys <yskrmt@gmail.com>
2024-05-07 22:50:28 +0900

    Load and save a note - 3

--- Notes/NotePage.xaml.cs
+++ Notes/NotePage.xaml.cs
@@ -10,4 +10,18 @@ public partial class NotePage : ContentPage
 		if (File.Exists(_fileName))
 			TextEditor.Text = File.ReadAllText(_fileName);
 	}
+	private void SaveButton_Clicked(object sender, EventArgs e)
+	{
+		// Save the file.
+		File.WriteAllText(_fileName, TextEditor.Text);
+	}
+
+	private void DeleteButton_Clicked(object sender, EventArgs e)
+	{
+		// Delete the file.
+		if (File.Exists(_fileName))
+			File.Delete(_fileName);
+
+		TextEditor.Text = string.Empty;
+	}
 }
